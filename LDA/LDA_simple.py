defa64c4231703ff05ba0606b35823fe2391db73
Eli-Boaz Clapper <74557493+EliClapper@users.noreply.github.com>
2022-04-02 22:12:51 +0200

    Update LDA_simple.py
    variable for K

--- LDA/LDA_simple.py
+++ LDA/LDA_simple.py
@@ -1,38 +1,37 @@
 import numpy as np
 import pandas as pd
 
-# NOTE, algorithm not yet generalized to variable K and p.
+#Linear Discriminant Analysis for K groups. This example uses 3 groups, but any number can be used.
 
 # generate data from normal dist
 n = 100                 # sample size
 np.random.seed(6164900) # set seed for random sampling
-group1 = np.random.normal(loc = 1, scale = 1, size= n)    # generate n values from normal with mean 0.5
-group2 = np.random.normal(loc = -1, scale = 1, size = n)  # generate n values from normal with mean -0.5
-X = pd.Series(np.concatenate([group1, group2]))       # append values to one pd series
-y = pd.Series(np.repeat([0,1] , repeats = n))         # create groups by repeating each element in list [0,1] 100 times
+group1 = np.random.normal(loc = -2, scale = 1, size= n)    # generate n values from normal with mean 0.5
+group2 = np.random.normal(loc = 0, scale = 1, size = n)  # generate n values from normal with mean -0.5
+group3 = np.random.normal(loc = 2, scale = 1, size = n)  # generate n values from normal with mean -0.5
+X = pd.Series(np.concatenate([group1, group2, group3]))       # append values to one pd series
+y = pd.Series(np.repeat([0,1,2] , repeats = n))         # create groups by repeating each element in list [0,1] 100 times
 df = pd.concat([y,X], axis = 1, keys= ['group', 'X']) # concatenate the two series to a dataframe
+
 
 # to get the discriminant function of x, we need the group means, probabilities and weighted variance
 def estimates(dataframe):
-    K = dataframe['group'].nunique()                  # number of groups is unique elements in group
-    group1 = dataframe[dataframe['group'] == 0]['X']  # obtain series for group 1
-    group2 = dataframe[dataframe['group'] == 1]['X']  # series with values for group 2
-    N =  len(group1) + len(group2)                    # total sample size
-    probs = pd.Series([len(group1) / N, len(group2) / N]) # obtain probability of being in a group
-    means = pd.Series([group1.mean(), group2.mean()])     # obtain series of group means
-
-    group1_diffsq = ((group1 - means[0])**2).sum()        # get squared differences for group 1
-    group2_diffsq = ((group2 - means[1])**2).sum()        # for group2
-
-    weighted_Var = (group1_diffsq + group2_diffsq) / (N-K) # obtain weighted variance
-
-    return([means, weighted_Var, probs])                    # return list of lists
-
+    levels = dataframe['group'].unique() # unqiue levels in group
+    K = len(levels)               # how many groups in total 
+    GroupList = [0]*K             # initalize list of length K
+    for i in levels:              # Put X values of all K groups in GruopList
+        GroupList[i] = dataframe[dataframe['group'] == i]
+    N = sum([len(group) for group in GroupList])    # obtain total sample size
+    Probs = [(len(group)/N) for group in GroupList] # obtain probabilities of being in a group
+    Means = [group.mean().tolist()[1] for group in GroupList] # obtain mean for all groups
+    Diffsqs = [((group - mean)**2).sum().tolist()[1] for group, mean in zip(GroupList, Means)] # obtain squared difference sum in all groups
+    WeightedVar = sum(Diffsqs) / (N-K) # obtain weighted variance
+    return([Means, WeightedVar, Probs]) # return list of metrics we need
 
 #discrimination function
 def deltax(value, dataframe):
     ests = estimates(dataframe)     # obtain estimates
-    dx = value * (ests[0] / ests[1] - (ests[0]**2)/ (2*ests[1])) + np.log(ests[2]) # obtain discrimination estimate for both groups
+    dx = [(value * (mean / ests[1]) - (mean**2 / (2*ests[1])) + np.log(prob)) for mean, prob in zip(ests[0], ests[2])]
     return(dx) # return the discrimination estimates
 
 # function to loop over df
@@ -40,7 +39,7 @@ def simple_LDA(dataframe):
     preds = []                   # initiate memory
     for value in dataframe['X']: # loop over all values in the dataframe
         disc_vals = deltax(value, dataframe) # get discrimination values
-        group = int(disc_vals[disc_vals == max(disc_vals)].index.values) #get index of where disc val is largest
+        group = np.argmax(disc_vals) #get index of where disc val is largest
         preds.append(group) #append that index to predicted values
     return(preds)
 
